3a4293d8a0c17c175ef2a0cb11dd99a5cfe3a904
front-end-performer <sodolinskyi@gmail.com>
2024-11-27 17:41:09 +0100

    chnaged name

---
+++ packages/acroButton/__tests__/acroButton.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const acroButton = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(acroButton(), 'Hello from acroButton');
+console.info('acroButton tests passed');
---
+++ packages/acroButton/lib/acroButton.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = button;
+
+function acroButton() {
+  return 'Hello from acroButton';
+}
